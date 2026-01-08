c99cbea603f435856f0d4dc98a3308a1f1b18501
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-08 11:04:32 -0500

    Completed test login success

--- e2e/test_auth.py
+++ e2e/test_auth.py
@@ -1,25 +1,25 @@
 from .conftest import page, test_user
 
-local_url = "http://127.0.0.1:8000/"
+url = "http://127.0.0.1:8000/"
 
 def test_auth_page_loads(page, live_server):
-    page.goto(local_url + "login/")
+    page.goto(url + "login/")
     assert page.title() != ""
 
 def test_login_form_exists(page, live_server):
-    page.goto(local_url + "login/")
+    page.goto(url + "login/")
 
     assert page.locator("input[name='username']").is_visible()
     assert page.locator("input[name='password']").is_visible()
     assert page.locator("button[type='submit']").is_visible()
 
 def test_login_success(page, test_user, live_server):
-    page.goto(local_url + "login/")
+    page.goto(url + "login/")
 
     page.fill("input[name='username']", "testuser")
     page.fill("input[name='password']", "testpassword")
     page.click("button[type='submit']")
 
-    page.wait_for_url(local_url, timeout=5000)
+    page.wait_for_url(url, timeout=5000)
 
     assert "login" not in page.url
