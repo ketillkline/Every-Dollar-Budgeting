7ad95c60fc0e2d76b938bafe9a30fc7391863ebe
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-08 10:54:46 -0500

    Continued work on testing the login page success with a test user

--- e2e/test_auth.py
+++ e2e/test_auth.py
@@ -1,14 +1,23 @@
-from .imports import page, local_url
+from .conftest import page, test_user
 
-def test_auth_page_loads(page):
-    page.goto(local_url + "login/")
+def test_auth_page_loads(page, live_server):
+    page.goto(live_server.url + "/login/")
     assert page.title() != ""
 
-def test_login_form_exists(page):
-    page.goto(local_url + "login/")
+def test_login_form_exists(page, live_server):
+    page.goto(live_server.url + "/login/")
 
     assert page.locator("input[name='username']").is_visible()
     assert page.locator("input[name='password']").is_visible()
     assert page.locator("button[type='submit']").is_visible()
 
+def test_login_success(page, test_user, live_server):
+    page.goto(live_server.url + "/login/")
 
+    page.fill("input[name='username']", "testuser")
+    page.fill("input[name='password']", "testpassword")
+    page.click("button[type='submit']")
+
+    page.wait_for_url(live_server.url + "/")
+
+    assert page.url == live_server.url + "/"
