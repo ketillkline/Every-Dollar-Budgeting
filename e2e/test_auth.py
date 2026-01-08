8f5f9216c3246b91ebfd6ee8cdd9855486f33fd2
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-08 10:07:39 -0500

    Created a test to check that the login form exists

--- e2e/test_auth.py
+++ e2e/test_auth.py
@@ -3,3 +3,12 @@ from .imports import page, local_url
 def test_auth_page_loads(page):
     page.goto(local_url + "login/")
     assert page.title() != ""
+
+def test_login_form_exists(page):
+    page.goto(local_url + "login/")
+
+    assert page.locator("input[name='username']").is_visible()
+    assert page.locator("input[name='password']").is_visible()
+    assert page.locator("button[type='submit']").is_visible()
+
+
