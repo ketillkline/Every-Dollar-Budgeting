7e44428060f825865c3fa502d74e3aab44364539
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-14 13:09:10 -0500

    Fixed password YAML and added static file root to settings.py

--- SimpleBudget/settings.py
+++ SimpleBudget/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "accounts" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
