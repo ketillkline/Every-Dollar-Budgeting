77018c5b7896fb7634320ad1465831ebe5486412
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-14 12:56:56 -0500

    Fixed password validator

--- accounts/validators.py
+++ accounts/validators.py
@@ -16,3 +16,7 @@ class StrongPasswordValidator:
 
         if not re.search(r"[!@#$%^&*()-+{}"':;><.,?/]', password):
             raise ValidationError (_("Password must contain 1 special character"))
+
+    def get_help_text(self):
+        return _(
+            "Your password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")
